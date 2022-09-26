925a6c38379d0ba56af8c7a7dd4db3f32e7312af
konvikkor <konvikkor@mail.ru>
2022-09-26 22:32:06 +0600

    Update jsStruct.js
    Разбор и добавление получения списка типов

--- rootHTML/jsModule/jsStruct.js
+++ rootHTML/jsModule/jsStruct.js
@@ -12,36 +12,62 @@ class TType {
  * Список типов и их размер
  */
 class TTypeList {
+    /** @type {Map<String,TType>} */
     #list = new Map();
     /** @param {TType} type @returns {TTypeList}*/
     set add(type) { if (type.constructor.name == 'TType') { this.#list.set(type.name, type); } else { throw "Нужный тип данных TType"; }; return this; }
     constructor() { }
+    printList() {
+        let printTable = [];
+        for (const item of this.#list.keys()) {
+            printTable.push({ item: item, size: this.#list.get(item).size })
+        }
+        console.table(printTable);
+    }
+    [Symbol.iterator](param) {
+        console.log(`iterator:`, param);
+        let n = 0;
+        let done = false;
+        console.log(this.#list.values());
+        return {
+            next() {
+                if (n == 100) { done = true }
+                n += 10;
+                return { value: n, done: done };
+            },
+            return() {
+                return { done: true };
+            }
+        };
+    }
 }
 const TypeList = new TTypeList();
 
 let sizeType = 0;
-TypeList.add = new TType('void',0);
+TypeList.add = new TType('void', 0);
 sizeType = 1;
-for (const typwName of ['byte','char','TINYINT','BOOL','BOOLEAN','INT1','int8','uint8']) {
-    TypeList.add = new TType(typwName,sizeType);
+for (const typwName of ['byte', 'char', 'TINYINT', 'BOOL', 'BOOLEAN', 'INT1', 'int8', 'uint8']) {
+    TypeList.add = new TType(typwName, sizeType);
 }
 sizeType *= 2; /** 2 */
-for (const typwName of ['wchar_t','int16','uint16','short int','unsigned short int', 'signed short int']) {
-    TypeList.add = new TType(typwName,sizeType);
+for (const typwName of ['wchar_t', 'int16', 'uint16', 'short int', 'unsigned short int', 'signed short int']) {
+    TypeList.add = new TType(typwName, sizeType);
 }
 sizeType *= 2; /** 4 */
-for (const typwName of ['float','int32','uint32','int','unsigned int','signed int']) {
-    TypeList.add = new TType(typwName,sizeType);
+for (const typwName of ['float', 'int32', 'uint32', 'int', 'unsigned int', 'signed int']) {
+    TypeList.add = new TType(typwName, sizeType);
 }
 sizeType *= 2; /** 8 */
-for (const typwName of ['double','unsigned long long int','long long int','unsigned long int','signed long int','long int']) {
-    TypeList.add = new TType(typwName,sizeType);
+for (const typwName of ['double', 'unsigned long long int', 'long long int', 'unsigned long int', 'signed long int', 'long int']) {
+    TypeList.add = new TType(typwName, sizeType);
 }
 sizeType = 12; /** 12 */
 for (const typwName of ['long double']) {
-    TypeList.add = new TType(typwName,sizeType);
+    TypeList.add = new TType(typwName, sizeType);
 }
 sizeType = undefined;
+TypeList.printList();
+console.log([...TypeList]);
 
 class TStruct extends EventTarget {
     constructor(name = '') {
