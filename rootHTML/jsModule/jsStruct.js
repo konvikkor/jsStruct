c1fe4976fc21dd0bb2eb52739740e26dbbae54d6
konvikkor <konvikkor@mail.ru>
2023-01-22 03:00:26 +0600

    Размышления о важном

--- rootHTML/jsModule/jsStruct.js
+++ rootHTML/jsModule/jsStruct.js
@@ -13,41 +13,77 @@ signed long long: -9223372036854775807...9223372036854775807
 unsigned long long: 0...18446744073709551615 (= 264−1)
 */
 
+/**
+ * @typedef {{name:string,type:string}} StructFields
+ */
+
 /** @type {Map<String,JSType | JSStruct>} */
-var GlobalListTypes = new Map();
+export const JSTypesList = new Map();
 
 export class JSType extends EventTarget {
     #name; #size;
     get name() { return this.name };
     get size() { return this.#size };
     /** @param {CustomEvent} event */
-    #setSize(event) {
-        let { detail } = event;
-        this.#size = Number(detail) ?? 0;
-    }
+    #setSize(event) { let { detail } = event; this.#size = Number(detail) ?? 0; }
     /** @param {String} name @param {Number?} size */
     constructor(name, size = 0) {
         super();
-        if (GlobalListTypes.has(name)){ throw 'Data type already registered'; };
+        if (JSTypesList.has(name)) { throw 'Data type already registered'; };
         this.#name = name;
         this.#size = size;
         this.addEventListener('setSize', this.#setSize);
-        GlobalListTypes.set(this.#name, this);
+        JSTypesList.set(this.#name, this);
     }
 }
 
 export class JSStruct extends JSType {
     #rootFields;
-    /** @param {String} name @param {[JSType|JSStruct]} ArrayTypes */
+    /** @param {String} name @param {[StructFields]} ArrayTypes */
     constructor(name, ArrayTypes) {
         super(name, 0);
-        this.#rootFields = ArrayTypes.slice();
+        this.#rootFields = ArrayTypes.filter((v) => { return (v.name != '' && v.type != '') }).slice().map(v => {
+            let itemType = JSTypesList.get(v.type);
+            if (itemType.constructor.name == 'JSType' || itemType.constructor.name == 'JSStruct') { v.type = itemType; };
+            return v;
+        });
         let localTotalSize = 0;
-        for (const itemType of ArrayTypes) {
-            if (itemType.constructor.name == 'JSType' || itemType.constructor.name == 'JSStruct') {
-                localTotalSize = localTotalSize + itemType.size;
+        for (let item of ArrayTypes) {
+            if (Object.keys(item).length != 2) { continue; }
+            if (item.type.constructor.name == 'JSType' || item.type.constructor.name == 'JSStruct') {
+                localTotalSize = localTotalSize + item.type.size;
             };
         };
         this.dispatchEvent(new CustomEvent('setSize', { detail: localTotalSize }));
+    }
+    [Symbol.iterator]() {
+        let i = 0;
+        return {
+            next: () => {
+                if (this.#rootFields.length > i) {
+                    let localVar = this.#rootFields[i];
+                    i++;
+                    return { done: false, value: localVar }
+                } else {
+                    return { done: true}
+                }
+            }
+        }
+    }
+}
+
+export class JSRegister {
+    static type(name, size = 0) { }
+    static struct(name, fields) { }
+}
+
+export class JSVariable extends EventTarget {
+    #localJSTypes; #name;
+    constructor(variableName, JSTypeName) {
+        super();
+        this.#name = variableName;
+        if (JSTypesList.has(JSTypeName)) {
+            this.#localJSTypes = JSTypesList.get(JSTypeName);
+        }
     }
 }
--- rootHTML/test.js
+++ rootHTML/test.js
@@ -1,9 +1,20 @@
-import { JSStruct, JSType } from "./jsModule/jsStruct.js";
+import { JSStruct, JSType, JSTypesList } from "./jsModule/jsStruct.js";
 
 let test1JSType = new JSType('int8',1);
 console.log(test1JSType,test1JSType.size);
 let test2JSType = new JSType('int16',2);
 console.log(test2JSType,test2JSType.size);
+let test3JSType = new JSType('int32',4);
+console.log(test3JSType,test3JSType.size);
+let test4JSType = new JSType('int64',8);
+console.log(test4JSType,test4JSType.size);
 
-let testJSStruct = new JSStruct('myStruct',[test1JSType,test2JSType]);
+let testJSStruct = new JSStruct('myStruct',[
+    {name:'test',type:'int8'},
+    {name:'test2',type:'int16'}
+]);
 console.log(testJSStruct, testJSStruct.size);
+console.log(JSTypesList);
+for (const item of testJSStruct) {
+    console.log(item)
+}
